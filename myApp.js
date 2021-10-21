07f22c9748604c1f72835c58dc3bbf36c23b30e2
kstev <ksteven8646@student.gwinnetttech.edu>
2021-10-21 13:02:04 -0400

    Sends the /views/index.html file as a response to GET requests to the / path.

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
- res.send('Hello Express')
-});
+// 1) Meet the node console
+// console.log("Hello World");
 
+// 2) Start a Working Express Server
+// app.get('/', function (req, res) {
+//  res.send('Hello Express');
+// });
+
+// 3) Server an HTML File
+app.get("/", function(req, res) {
+ res.sendFile(__dirname + "/views/index.html");
+});
 
 module.exports = app;
